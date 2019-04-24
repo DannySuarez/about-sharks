7c7272ba0dadb0b08924bccb483374eceb691cee
DannySuarez <suarezd10@gmail.com>
2019-04-24 08:48:26 -0700

    imported checkAnswers into index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import checkAnswers from './checkAnswers.js';
+
 const question1Input = document.getElementById('question1');
 const question2Input = document.getElementById('question2');
 const question3Input = document.getElementById('question3');
@@ -8,12 +10,16 @@ const button = document.getElementById('submit');
 
 button.addEventListener('click', event => {
     event.preventDefault();
-    const question1Answer = question1Input;
-    const question2Answer = question2Input;
-    const question3Answer = question3Input;
-    const question4Answer = question4Input;
-    const question5Answer = question5Input;
+    const question1Answer = question1Input.value.trim();
+    const question2Answer = question2Input.value.trim();
+    const question3Answer = question3Input.value.trim();
+    const question4Answer = question4Input.value.trim();
+    const question5Answer = question5Input.value.trim();
 
+    const score = checkAnswers(question1Answer, question2Answer, question3Answer, question4Answer, question5Answer);
 
+    const message = 'Your score: ' + score + ' out of 5!';
+    
+    result.textContent = message;
 
 });
