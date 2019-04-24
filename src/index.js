6f1bdc9387f2c3a19c22a434da7c40b376ac211e
DannySuarez <suarezd10@gmail.com>
2019-04-23 17:06:18 -0700

    created variables for each quiz input

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const question1Input = document.getElementById('question1');
+const question2Input = document.getElementById('question2');
+const question3Input = document.getElementById('question3');
+const question4Input = document.getElementById('question4');
+const question5Input = document.getElementById('question5');
+const result = document.getElementById('score-result');
+const button = document.getElementById('submit');
+
