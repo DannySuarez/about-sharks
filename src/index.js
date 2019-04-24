0af0f6ea089acfc61396b61d0c87793691b4696d
DannySuarez <suarezd10@gmail.com>
2019-04-23 17:35:30 -0700

    created variables to hold input values

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ const question5Input = document.getElementById('question5');
 const result = document.getElementById('score-result');
 const button = document.getElementById('submit');
 
+button.addEventListener('click', event => {
+    event.preventDefault();
+    const question1Answer = question1Input;
+    const question2Answer = question2Input;
+    const question3Answer = question3Input;
+    const question4Answer = question4Input;
+    const question5Answer = question5Input;
+
+
+
+});
